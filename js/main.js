5af342ee6d52d5e9cef05b22e65dbac0db1b6929
dukeputu <94835906+dukeputu@users.noreply.github.com>
2022-08-23 11:16:19 +0530

    get array in 5 list

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 
+const nameArray =[];
+
 document.getElementById('event-delegate').addEventListener('click', function (event) {
     const e = event.target.parentNode;
     const nodeLists = e.childNodes.item(1);
     const playerNames = nodeLists.innerText;
-    console.log(playerNames);
+    //*************** */
+     nameArray.push(playerNames);
+ 
+     if(nameArray.length >5){
+        alert('hi')
+        error
+       
+     }
+
+     document.getElementById('pleyers-numbers').innerText = nameArray.length,
+    console.log(nameArray);
+    console.log(nameArray.length);
+
+    
+   
+
 })
+
 
